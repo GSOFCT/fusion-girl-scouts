4d1b7198b14d9593708dce1c29abb497a4de4f44
spruibal <stefania.ruibal@trincoll.edu>
2016-04-18 14:55:32 -0400

    Update maps_lib.js
    changed zoom and center

--- js/maps_lib.js
+++ js/maps_lib.js
@@ -40,14 +40,14 @@ var MapsLib = {
   //example: locationColumn:     "'my location'",
   locationColumn:     "Address",
 
-  map_centroid:       new google.maps.LatLng(41.7682,-72.684), //center that your map defaults to
+  map_centroid:       new google.maps.LatLng(41.7682,-70.684), //center that your map defaults to
   locationScope:      "connecticut",      //geographical area appended to all address searches
   recordName:         "result",       //for showing number of results
   recordNamePlural:   "results",
 
 
   searchRadius:       805,            //in meters ~ 1/2 mile
-  defaultZoom:        9,             //zoom level when map is loaded (bigger is more zoomed in)
+  defaultZoom:        8,             //zoom level when map is loaded (bigger is more zoomed in)
   addrMarkerImage:    'images/blue-pushpin.png', // set to empty '' to hide searched address marker
   currentPinpoint:    null,
 
